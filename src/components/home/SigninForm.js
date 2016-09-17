c641b40c05faf306daffb45df61083b53646b900
spondbob <spondbob@eamca.com>
2016-09-17 16:18:26 +1000

    implement redux-form

--- src/components/home/SigninForm.js
+++ src/components/home/SigninForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form';
 
 class SigninForm extends Component {
   render() {
@@ -6,13 +7,29 @@ class SigninForm extends Component {
       <form className="form-signin">
         <h2 className="form-signin-heading">Please sign in</h2>
         <label htmlFor="inputEmail" className="sr-only">Email address</label>
-        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required />
+        <Field type="email"
+          name="inputEmail"
+          id="inputEmail"
+          className="form-control"
+          placeholder="Email address"
+          component="input"
+          required />
         <label htmlFor="inputPassword" className="sr-only">Password</label>
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+        <Field type="password"
+          name="inputPassword"
+          id="inputPassword"
+          className="form-control"
+          placeholder="Password"
+          component="input"
+          required />
         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
     )
   }
 }
+
+SigninForm = reduxForm({
+  form: 'signin'
+})(SigninForm);
 
 export default SigninForm
