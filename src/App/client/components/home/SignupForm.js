57a9cc9db014f9a5c839b91ae0f7c06cbd09d091
spondbob <spondbob@eamca.com>
2016-09-18 23:35:44 +1000

    enable redux-form

--- src/App/client/components/home/SignupForm.js
+++ src/App/client/components/home/SignupForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router'
 
 class SignupForm extends Component {
@@ -6,16 +7,38 @@ class SignupForm extends Component {
     return (
       <form className="form-signin">
         <label htmlFor="inputName" className="sr-only">Name</label>
-        <input type="email" id="inputName" className="form-control" placeholder="Name" required />
+        <Field type="text"
+          name="inputName"
+          id="inputName"
+          className="form-control"
+          placeholder="Name"
+          component="input"
+          required />
         <label htmlFor="inputEmail" className="sr-only">Email address</label>
-        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required />
+        <Field type="email"
+          name="inputEmail"
+          id="inputEmail"
+          className="form-control"
+          placeholder="Email address"
+          component="input"
+          required />
         <label htmlFor="inputPassword" className="sr-only">Password</label>
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+        <Field type="password"
+          name="inputPassword"
+          id="inputPassword"
+          className="form-control"
+          placeholder="Password"
+          component="input"
+          required />
         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
         <Link to="/" className="btn btn-lg btn-default btn-block">Have an account? Sign in</Link>
       </form>
     )
   }
 }
+
+SignupForm = reduxForm({
+  form: 'signup'
+})(SignupForm);
 
 export default SignupForm
