ebd8738d18006a1ef5526e36d83072c21eec1a82
spondbob <spondbob@eamca.com>
2016-09-19 19:49:51 +1000

    add testing cases

---
+++ test/redux/user.test.js
@@ -0,0 +1,93 @@
+import { expect } from 'chai'
+import { Map } from 'immutable';
+import * as actions from '../../src/client/actions/usersAction';
+import * as reducers from '../../src/client/reducers/usersReducer';
+import * as types from '../../src/client/constants/usersConst';
+
+describe('[Redux] - User: actions', () => {
+  it('should create an action when signing up', () => {
+    const expectedAction = {
+      type: types.SIGNUP_REQUEST
+    }
+    expect(actions.signUpRequest()).to.deep.equal(expectedAction);
+  });
+
+  it('should create an action when successfully signed up', () => {
+    const expectedAction = {
+      type: types.SIGNUP_SUCCESS
+    };
+    expect(actions.signUpSuccess()).to.deep.equal(expectedAction);
+  });
+
+  it('should create an action when sign up is failed', () => {
+    const error = {
+      reason: "error message"
+    };
+    const expectedAction = {
+      type: types.SIGNUP_FAILURE,
+      error: {
+        reason: "error message"
+      }
+    };
+    expect(actions.signUpFailure(error)).to.deep.equal(expectedAction);
+  });
+});
+
+describe('[Redux] - User: reducers', () => {
+  it('should return the initial state', () => {
+    const expectedReducer = {
+      isAuthenticated: false,
+      isAuthenticating: false,
+      isSignupFail: false,
+      statusText: null
+    }
+    expect(
+      reducers.default(undefined, {})
+    ).to.deep.equal(expectedReducer);
+  });
+
+  it('should handle SIGNUP_REQUEST', () => {
+    const expectedReducer = {
+      isAuthenticated: false,
+      isAuthenticating: false,
+      isSignupFail: false,
+      statusText: null
+    }
+    expect(
+      reducers.default(undefined, {
+        type: types.SIGNUP_REQUEST
+      })
+    ).to.deep.equal(expectedReducer);
+  });
+
+  it('should handle SIGNUP_SUCCESS', () => {
+    const expectedReducer = {
+      isAuthenticated: false,
+      isAuthenticating: false,
+      isSignupFail: false,
+      statusText: null
+    }
+    expect(
+      reducers.default(undefined, {
+        type: types.SIGNUP_SUCCESS
+      })
+    ).to.deep.equal(expectedReducer);
+  });
+
+  it('should handle SIGNUP_FAILURE', () => {
+    const expectedReducer = {
+      isAuthenticated: false,
+      isAuthenticating: false,
+      isSignupFail: true,
+      statusText: "error message"
+    }
+    expect(
+      reducers.default(undefined, {
+        type: types.SIGNUP_FAILURE,
+        error: {
+          reason: "error message"
+        }
+      })
+    ).to.deep.equal(expectedReducer);
+  });
+});
